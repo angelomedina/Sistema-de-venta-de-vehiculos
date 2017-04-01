0297fb96b64a8393be1f074e2a3d01960e1f81ff
angelomedina <angelomedinabarraza@gmail.com>
2017-03-31 21:12:09 -0600

    Se crearon paketes

--- Primer Proyecto POO Venta de Vehiculos/src/Main/Main.java
+++ Primer Proyecto POO Venta de Vehiculos/src/Main/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package primer.proyecto.poo.venta.de.vehiculos;
+package Main;
 
 /**
  *
  * @author Usuario
  */
-public class PrimerProyectoPOOVentaDeVehiculos {
+public class Main {
 
     /**
      * @param args the command line arguments
