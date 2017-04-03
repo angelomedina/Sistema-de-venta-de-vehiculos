79c0a931953e0a1e288928ffdffe7667cb75cb18
angelomedina <angelomedinabarraza@gmail.com>
2017-04-02 23:39:32 -0600

    Centrar Pantalla

--- Primer Proyecto POO Venta de Vehiculos/src/Grafics/SellerAsAgency.java
+++ Primer Proyecto POO Venta de Vehiculos/src/Grafics/SellerAsAgency.java
@@ -19,6 +19,8 @@ public class SellerAsAgency extends javax.swing.JFrame {
      */
     public SellerAsAgency() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
     }
 
     /**
